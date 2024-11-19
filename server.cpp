77fe057b0db5172012523384e26cf0602161305f
MD. Shimul <mdshemul480@gmail.com>
2024-11-20 00:41:47 +0600

    adding request handler in server

--- server.cpp
+++ server.cpp
@@ -10,6 +10,7 @@
 #include <cassert>
 
 #define PORT 5000
+const size_t K_MAX_MSG = 4096;
 
 static void msg(const char *msg)
 {
@@ -21,24 +22,6 @@ static void die(const char *msg)
     int err = errno;
     fprintf(stderr, "[%d] %s: %s\n", err, msg, strerror(err));
     abort();
-}
-
-static void do_something(int connfd)
-{
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0)
-    {
-        msg("read() error");
-        return;
-    }
-    printf("client says: %s\n", rbuf);
-
-    char wbuf[] = "world";
-    if (write(connfd, wbuf, strlen(wbuf)) < 0)
-    {
-        die("can't write on sock");
-    }
 }
 
 static int32_t read_full(int fd, char *buf, size_t n)
@@ -78,6 +61,49 @@ static int32_t write_all(int fd, char *buf, size_t n)
     return 0;
 }
 
+static int32_t one_request(int connfd)
+{
+    char rbuf[4 + K_MAX_MSG + 1];
+
+    errno = 0;
+
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+        {
+            msg("EOF");
+        }
+        else
+        {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+
+    if (err)
+    {
+        msg("read() error");
+        return err;
+    }
+
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    const char reply[] = "word";
+
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+
+    return write_all(connfd, wbuf, 4 + len);
+}
+
 int main()
 {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -112,8 +138,14 @@ int main()
         {
             continue;
         }
-
-        do_something(connfd);
+        while (true)
+        {
+            int32_t err = one_request(connfd);
+            if (err)
+            {
+                break;
+            }
+        }
         close(connfd);
     }
 
